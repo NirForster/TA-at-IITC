b1570408c3766401d865f8f67c33feb1472b822d
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-11-26 00:00:33 +0200

    Initialize my-project with Vite and React; set up Tailwind CSS, ESLint, and basic routing

--- 2024/November/24-11-24/last-core-hooks/src/App.jsx
+++ 2024/November/24-11-24/last-core-hooks/src/App.jsx
@@ -1,26 +1,36 @@
-
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 // Pages Import 
 import Home from './pages/Home.jsx'
 import SignInPage from './pages/SignInPage.jsx'
 import QueryPrint from './pages/QueryPrint.jsx'
-import DummyDataPage from './pages/DummyDataPage.jsx'
+import DummyDataPage, { fetchDataFn } from './pages/DummyDataPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/sign-in",
+    element: <SignInPage />
+  },
+  {
+    path: "/query-print",
+    element: <QueryPrint />
+  },
+  {
+    path: "/dummy",
+    element: <DummyDataPage />,
+    loader: fetchDataFn
+  }
+]);
 
 function App() {
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/query-print" element={<QueryPrint />} />
-        <Route path="/dummy" element={<DummyDataPage />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   )
 }
 
 export default App
-
