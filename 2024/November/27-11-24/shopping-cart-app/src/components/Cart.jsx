503150bcd1a83068770ccadac09248eee3e9bb97
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-11-27 11:57:25 +0200

    Enhance cart functionality by adding item removal and updating initial state management

--- 2024/November/27-11-24/shopping-cart-app/src/components/Cart.jsx
+++ 2024/November/27-11-24/shopping-cart-app/src/components/Cart.jsx
@@ -1,14 +1,29 @@
-import { useSelector } from "react-redux"
-
+import { useSelector, useDispatch } from "react-redux"
+import { removeItem } from "../store/slices/cartSlice";
 const Cart = () => {
+    const dispatch = useDispatch()
     const cart = useSelector((state) => state.cart)
     console.log(cart);
     
+    function handleRemoveItem(id) {
+        dispatch(removeItem(id))
+    }
+
     return (
         <div>
             <h2>My Cart</h2>
             <p>Total Item: {cart.totalQuantity}</p>
             <p>Total Price: {cart.totalPrice}</p>
+            <ul>
+                {cart.items.map((item) => (
+                    <li key={item.id} style={{margin: '10px 0'}}>
+                        <span>
+                            {item.name}: {item.price}₪ x {item.quantity} = ₪{item.totalItemPrice}
+                        </span>
+                        <button onClick={() => handleRemoveItem(item.id)}>Remove Item</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
--- 2024/November/27-11-24/shopping-cart-app/src/store/slices/cartSlice.js
+++ 2024/November/27-11-24/shopping-cart-app/src/store/slices/cartSlice.js
@@ -1,30 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialItems = [
-    { id: 1234, name: "Coca Cola Bottle", price: 8.90, quantity: 3, totalItemPrice: 26.70 },
-    { id: 5849, name: "Mana-Hama", price: 7, quantity: 3, totalItemPrice: 21 }
-];
+// const initialItems = [
+//     { id: 1234, name: "Coca Cola Bottle", price: 8.90, quantity: 3, totalItemPrice: 26.70 },
+//     { id: 5849, name: "Mana-Hama", price: 7, quantity: 3, totalItemPrice: 21 }
+// ];
+
+// initialItems.reduce((acc, item) => acc + item.totalItemPrice, 0)
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: { items: initialItems, totalQuantity: 6, totalPrice: initialItems.reduce((acc, item) => acc + item.totalItemPrice, 0) },
-    reducers: {
-        addItem: (state, action) => {
-            state.items.push(action.payload)
-            state.totalQuantity++
-            state.totalPrice += action.payload.price
-        },
-        removeItem: (state, action) => {
-            // 1. Find Item Index
-            const itemIndex = state.items.findIndex((item) => item.id === action.payload)
+  name: "cart",
+  initialState: { items: [], totalQuantity: 0, totalPrice: 0 },
+  reducers: {
+    addItem: (state, action) => {
+      const existingItem = state.items.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingItem) {
+        console.log("in the cart");
+      }
 
-            // 2. Filter Index
-            const item = state.items[itemIndex]
-            state.totalQuantity -= item.quantity
-            state.totalPrice -= item.price
-        }
-    }
-})
+      state.items.push(action.payload);
+      state.totalQuantity++;
+      state.totalPrice += action.payload.price;
+    },
+    removeItem: (state, action) => {
+      // 1. Find Item Index
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
 
-export const { addItem, removeItem } = cartSlice.actions
-export default cartSlice.reducer
+      // 2. Filter Index
+      const item = state.items[itemIndex];
+      state.totalQuantity -= item.quantity;
+      state.totalPrice -= item.price;
+      state.items.splice(item, 1);
+    },
+  },
+});
+
+export const { addItem, removeItem } = cartSlice.actions;
+export default cartSlice.reducer;
