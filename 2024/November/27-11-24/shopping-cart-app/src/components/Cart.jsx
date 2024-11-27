3bbc041b08bc22a4f6219a58fbf56aeeaf166aaa
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-11-27 11:25:04 +0200

    Add cart display and product addition functionality in shopping cart app

--- 2024/November/27-11-24/shopping-cart-app/src/components/Cart.jsx
+++ 2024/November/27-11-24/shopping-cart-app/src/components/Cart.jsx
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux"
+
 const Cart = () => {
+    const cart = useSelector((state) => state.cart)
+    console.log(cart);
+    
     return (
         <div>
             <h2>My Cart</h2>
+            <p>Total Item: {cart.totalQuantity}</p>
+            <p>Total Price: {cart.totalPrice}</p>
         </div>
     )
 }
--- 2024/November/27-11-24/shopping-cart-app/src/components/Products.jsx
+++ 2024/November/27-11-24/shopping-cart-app/src/components/Products.jsx
@@ -4,10 +4,34 @@ const products = [
     { id: 3, name: 'Headphones', price: 100 },
 ];
 
+// Redux Imports
+import { addItem } from '../store/slices/cartSlice.js'
+import { useDispatch } from "react-redux";
+
 const Products = () => {
+    const dispatch = useDispatch()
+
+    function handleAddItem(item) {
+        const itemData = {
+            ...item,
+            quantity: 1,
+            totalItemPrice: item.price
+        }
+
+        dispatch(addItem(itemData))
+    }
+
     return (
         <div>
             <h2>Products List</h2>
+            {products.map((prod) => (
+                <div key={prod.id}>
+                    <span>
+                        {prod.name} - {prod.price}₪
+                    </span>
+                    <button onClick={() => handleAddItem(prod)}>Add To Cart</button>
+                </div>
+            ))}
         </div>
     )
 }
