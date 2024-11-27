f2fd2fc1a5ed19f0c8a49e5dff1413c00c447836
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-11-27 11:40:40 +0200

    Refactor cartSlice to initialize totalPrice dynamically based on initialItems

--- 2024/November/27-11-24/shopping-cart-app/src/store/slices/cartSlice.js
+++ 2024/November/27-11-24/shopping-cart-app/src/store/slices/cartSlice.js
@@ -1,11 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialItems = [
+    { id: 1234, name: "Coca Cola Bottle", price: 8.90, quantity: 3, totalItemPrice: 26.70 },
+    { id: 5849, name: "Mana-Hama", price: 7, quantity: 3, totalItemPrice: 21 }
+];
+
 const cartSlice = createSlice({
     name: 'cart',
-    initialState: { items: [
-        { id: 1234, name: "Coca Cola Bottle", price: 8.90, quantity: 3, totalItemPrice: 26.70},
-        { id: 5849, name: "Mana-Hama", price: 7, quantity: 3, totalItemPrice: 21 }
-    ], totalQuantity: 6, totalPrice: 0},
+    initialState: { items: initialItems, totalQuantity: 6, totalPrice: initialItems.reduce((acc, item) => acc + item.totalItemPrice, 0) },
     reducers: {
         addItem: (state, action) => {
             state.items.push(action.payload)
