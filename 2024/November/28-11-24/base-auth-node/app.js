cbf2ea4274c489d78c911875f41c529cf8c08e89
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-11-28 10:40:07 +0200

    Implement sign-up endpoint with password hashing and improve code formatting

--- 2024/November/28-11-24/base-auth-node/app.js
+++ 2024/November/28-11-24/base-auth-node/app.js
@@ -1,27 +1,40 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
 const PORT = 3000;
 const saltRounds = 10;
+const hashKey = "PeogG3lBZpSErxuBgrAA0Y6ermcD04XGGeTn7uUYfLvOFEvdaW"
 
-app.use(express.json())
+const dummyUser = {
 
-app.get('/', (req, res) => {
-    res.status(200).send({
-        message: "Hello from the server!"
-    })
-})
+}
 
-app.post('/encrypt-password', (req, res) => {
-    const { userPassword } = req.body;
-    console.log(userPassword);
-    res.send({
-        userPassword
-    })
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Hello from the server!",
+  });
+});
+
+app.post("/sign-up", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // console.log(userPassword);
+
+    const hashedPassword = await bcrypt.hash(userPassword + hashKey, saltRounds);
+
+    return res.send({
+      hashedPassword,
+    });
+
+  } catch (error) {
+    res.status(500).send(error)
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-})
+  console.log(`Server is running on port: ${PORT}`);
+});
