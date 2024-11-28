0816e2b9245cb06bb69cfc6fe0007c0e0baecdff
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-11-28 10:52:52 +0200

    Add sign-in endpoint and fix password hashing in sign-up

--- 2024/November/28-11-24/base-auth-node/app.js
+++ 2024/November/28-11-24/base-auth-node/app.js
@@ -5,11 +5,9 @@ const app = express();
 
 const PORT = 3000;
 const saltRounds = 10;
-const hashKey = "PeogG3lBZpSErxuBgrAA0Y6ermcD04XGGeTn7uUYfLvOFEvdaW"
+const hashKey = "PeogG3lBZpSErxuBgrAA0Y6ermcD04XGGeTn7uUYfLvOFEvdaW";
 
-const dummyUser = {
-
-}
+const dummyUser = {};
 
 app.use(express.json());
 
@@ -24,17 +22,50 @@ app.post("/sign-up", async (req, res) => {
     const { email, password } = req.body;
     // console.log(userPassword);
 
-    const hashedPassword = await bcrypt.hash(userPassword + hashKey, saltRounds);
+    const hashedPassword = await bcrypt.hash(password + hashKey, saltRounds);
+
+    dummyUser.email = email;
+    dummyUser.password = hashedPassword;
 
     return res.send({
-      hashedPassword,
+      you: dummyUser,
     });
-
   } catch (error) {
-    res.status(500).send(error)
+    res.status(500).send(error);
   }
 });
 
+app.post("/sign-in", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // You task to first find the user in the DB
+
+    // Check the Email:
+    if (dummyUser.email !== email) {
+      return res.status(401).send("NO SUCH USER");
+    }
+
+    const isCorrectPassword = await bcrypt.compare(
+      password + hashKey,
+      dummyUser.password
+    );
+
+    if (isCorrectPassword) {
+      return res.status(200).send({
+        status: "success",
+        message: "Welcome",
+        data: dummyUser.email,
+      });
+    } else {
+      return res.status(401).send({
+        error: "Error",
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
