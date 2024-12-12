5da3fa11aee6857cf56aee9564c154886c640452
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-12-12 14:13:19 +0200

    Add jokes routing functionality; create jokesRoutes module and integrate with main app

--- 2024/December/12.12.2024/Server/app.js
+++ 2024/December/12.12.2024/Server/app.js
@@ -4,44 +4,12 @@ const dotenv = require("dotenv");
 
 const app = express();
 
-const jokes = [
-  {
-    baseline: "Why don’t skeletons fight each other?",
-    punchline: "They don’t have the guts.",
-  },
-  {
-    baseline: "What do you call cheese that isn't yours?",
-    punchline: "Nacho cheese.",
-  },
-  {
-    baseline: "Why couldn’t the bicycle stand up by itself?",
-    punchline: "It was two tired.",
-  },
-  { baseline: "What do you call a fake noodle?", punchline: "An impasta." },
-  { baseline: "Why don’t eggs tell jokes?", punchline: "They might crack up." },
-  {
-    baseline: "How does a penguin build its house?",
-    punchline: "Igloos it together.",
-  },
-  { baseline: "What do you call bears with no ears?", punchline: "B." },
-  {
-    baseline: "Why don’t some couples go to the gym?",
-    punchline: "Because some relationships don’t work out.",
-  },
-  {
-    baseline: "What did one wall say to the other?",
-    punchline: "I'll meet you at the corner.",
-  },
-  {
-    baseline: "Why did the scarecrow win an award?",
-    punchline: "Because he was outstanding in his field!",
-  },
-];
-
 app.use(express.json());
 dotenv.config();
 
-console.log(process.env);
+// Routes Imports
+const jokesRoutes = require("./routes/jokesRoutes");
+
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to MongoDB");
@@ -60,6 +28,9 @@ app.get("/jokes/random", (req, res) => {
   const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
   res.send(randomJoke);
 });
+
+app.use("/jokes", jokesRoutes);
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ 2024/December/12.12.2024/Server/routes/jokesRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const jokes = [
+  {
+    baseline: "Why don’t skeletons fight each other?",
+    punchline: "They don’t have the guts.",
+  },
+  {
+    baseline: "What do you call cheese that isn't yours?",
+    punchline: "Nacho cheese.",
+  },
+  {
+    baseline: "Why couldn’t the bicycle stand up by itself?",
+    punchline: "It was two tired.",
+  },
+  { baseline: "What do you call a fake noodle?", punchline: "An impasta." },
+  { baseline: "Why don’t eggs tell jokes?", punchline: "They might crack up." },
+  {
+    baseline: "How does a penguin build its house?",
+    punchline: "Igloos it together.",
+  },
+  { baseline: "What do you call bears with no ears?", punchline: "B." },
+  {
+    baseline: "Why don’t some couples go to the gym?",
+    punchline: "Because some relationships don’t work out.",
+  },
+  {
+    baseline: "What did one wall say to the other?",
+    punchline: "I'll meet you at the corner.",
+  },
+  {
+    baseline: "Why did the scarecrow win an award?",
+    punchline: "Because he was outstanding in his field!",
+  },
+];
+
+
+
+
+module.exports = router;
