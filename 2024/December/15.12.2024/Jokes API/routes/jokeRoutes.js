51a95587d605bdd7e0dfe4b1e5f57516085f04b6
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-12-15 15:48:55 +0200

    Refactor jokes API routes to use controller functions for improved readability and maintainability

--- 2024/December/15.12.2024/Jokes API/routes/jokeRoutes.js
+++ 2024/December/15.12.2024/Jokes API/routes/jokeRoutes.js
@@ -1,80 +1,27 @@
 const express = require('express');
-const Joke = require('../models/Joke.model');
+const {
+    getAllJokes,
+    createJoke,
+    getJokeById,
+    updateJoke,
+    deleteJoke,
+} = require('../controllers/jokesController.js');
+
 const router = express.Router();
 
-let jokes = [
-    { id: 1, joke: "Why don't scientists trust atoms? Because they make up everything!" },
-    { id: 2, joke: "Why did the scarecrow win an award? Because he was outstanding in his field!" }
-];
-
 // Get all jokes
-router.get('/', async (req, res) => {
-    try {
-        const jokes = await Joke.find();
-        res.json(jokes);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+router.get('/', getAllJokes);
 
 // Create a new joke
-router.post('/', async (req, res) => {
-    try {
-        console.log(req.body);
-        const result = await Joke.create(req.body);
-        console.log(result);
-        
-        res.status(201).json(result);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+router.post('/', createJoke);
 
 // Get a single joke by ID
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        
-        const joke = await Joke.findById(id);
-        if (!joke) throw new Error('Joke not found');
-        
-       return res.json(joke);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+router.get('/:id', getJokeById);
 
 // Update a joke by ID
-router.put('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const joke = await Joke.findByIdAndUpdate(id, req.body, { new: true });
-
-        if (!joke) throw new Error('Joke not found');
-
-        res.send({
-            message: 'Joke updated successfully',
-            joke
-        })
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+router.put('/:id', updateJoke);
 
 // Delete a joke by ID
-router.delete('/:id', async (req, res) => {
-   try {
-        const { id } = req.params;
-        const joke = await Joke.findByIdAndDelete(id);
-
-        if (!joke) throw new Error('Joke not found');
-
-        res.send({
-            message: 'Joke deleted successfully',
-        })
-   } catch (error) {
-    
-   }
-});
+router.delete('/:id', deleteJoke);
 
 module.exports = router;
