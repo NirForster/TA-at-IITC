b32b14c7abf9bc2997406ad9457bbfea7a6bc516
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-12-15 12:02:00 +0200

    Add Joke model; define schema with setup, punchline, likes, and createdAt fields

---
+++ 2024/December/15.12.2024/Jokes API/models/Joke.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const jokeSchema = new mongoose.Schema({
+  setup: {
+    type: String,
+    required: [true, "Setup is required!"],
+    minlength: 10,
+  },
+  punchline: {
+    type: String,
+    required: [true, "Punchline is required!"],
+    minlength: [3, "Punchline must be at least 3 characters long."],
+  },
+  likes: {
+    type: Number,
+    default: 0, // Default value if not provided
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now, // Automatically set the current date
+  },
+});
+
+module.exports = mongoose.model('Joke', jokeSchema);
