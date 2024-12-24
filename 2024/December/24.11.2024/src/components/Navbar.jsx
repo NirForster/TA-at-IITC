7e2c218d03e31965ccd68cbfbb902644c4a144fb
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-12-24 10:37:39 +0200

    Refactor Navbar component to use React Router's Link for navigation and add inline styles

--- 2024/December/24.11.2024/src/components/Navbar.jsx
+++ 2024/December/24.11.2024/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
-  return (
-    <nav>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/html">HTML</a> <a href="/css">CSS</a>
-      <a href="/js">JavaScript</a> <a href="/python">Python</a>
+return (
+    <nav style={{ background: '#333', padding: '10px' }}>
+        <Link to="/" style={{ margin: '0 10px', color: '#fff' }}>Home</Link>
+        <Link to="/about" style={{ margin: '0 10px', color: '#fff' }}>About</Link>
+        <Link to="/html" style={{ margin: '0 10px', color: '#fff' }}>HTML</Link>
+        <Link to="/css" style={{ margin: '0 10px', color: '#fff' }}>CSS</Link>
+        <Link to="/js" style={{ margin: '0 10px', color: '#fff' }}>JavaScript</Link>
+        <Link to="/python" style={{ margin: '0 10px', color: '#fff' }}>Python</Link>
     </nav>
-  );
+);
 };
 
 export default Navbar;
